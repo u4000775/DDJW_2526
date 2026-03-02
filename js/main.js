ba4df7793d5e6b7a10bd2f46d082cd7b748fa547
aescribano3 <alexescribanoreina@gmail.com>
2026-03-02 17:01:47 +0100

    Actualitzat DOM jquery

--- js/main.js
+++ js/main.js
@@ -1,30 +1,28 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
 
-        let player = prompt("Introdueix el nom del jugador","Jugador 1");
-		if (player === null || player.trim() === "") {
-			console.warn("Nom no valid");
-		} else {
-			console.log("Nom del jugador: " + player);
-		}
+var playButton = $('#play');
+playButton.on('click', function (){
+    let player = prompt("Introdueix el nom del jugador","Jugador 1");
+    if (player === null || player.trim() === "") {
+        console.warn("Nom no valid");
+    } else {
+        console.log("Nom del jugador: " + player);
+    }
 
-        window.location.assign("./html/game.html");
+    window.location.assign("./html/game.html");
+});
 
-    });
+var optionsButton = $('#options');
+optionsButton.on('click', function (){
+    console.error("Opció no implementada");
+});
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        console.error("Opció no implementada");
-    });
+var savesButton = $('#saves');
+savesButton.on('click', function (){
+    console.error("Opció no implementada");
+});
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        console.error("Opció no implementada");
-    });
-
-    document.getElementById('exit').addEventListener('click', 
-    function(){
-        console.warn("No es pot sortir!");
-    });
+var exitButton = $('#exit');
+exitButton.on('click', function (){
+    console.error("Opció no implementada");
 });
