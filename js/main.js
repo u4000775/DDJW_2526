a911444af6a3e192c9085319c3982cadeb40f76b
icarmonama <iker55fairmola@gmail.com>
2026-03-09 17:27:30 +0100

    Ajust final main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-import {$} from "../library/jquery-4.0.0.slim.module.min.js";
-
-$(function() {
-    $('#play').on('click', function() {
+addEventListener('load', function() {
+    document.getElementById('play').addEventListener('click', function() {
         let jugador = prompt("El teu nom:");
         console.log("Benvingut: " + jugador);
         alert("Comença la partida " + jugador);
+        window.location.assign("./html/game.html");
     });
 
-    $('#options').on('click', function() {
+    document.getElementById('options').addEventListener('click', function() {
         console.error("Opció no implementada");
     });
 
-    $('#saves').on('click', function() {
+    document.getElementById('saves').addEventListener('click', function() {
         console.error("Opció no implementada");
     });
 
-    $('#exit').on('click', function() {
+    document.getElementById('exit').addEventListener('click', function() {
         console.warn("No es pot sortir!");
     });
 });
