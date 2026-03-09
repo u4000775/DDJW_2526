a4fffbb99f5c35374e3b9ff0823dddf7663ca2f6
MaxBelfort <maxbelfort2005@gmail.com>
2026-03-09 16:27:17 +0100

    practica resolta

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        alert("Comença la partida");
-    });
+    document.getElementById('play').addEventListener('click', function(){
+        let nom = prompt("Nom del personatge:");
+		console.log("Iniciant partida amb el nom: " + nom);
+		alert("El teu nom de jugador és "+ nom + "!");
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    document.getElementById('options').addEventListener('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    document.getElementById('saves').addEventListener('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    document.getElementById('exit').addEventListener('click', function(){
         console.warn("No es pot sortir!");
     });
 });
