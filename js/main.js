710ca4dc7d9c5c05cca494d802107252b5e3ef86
u6105601 <u6105601@campus.udg.edu>
2026-03-09 17:58:12 +0100

    ISSUE #3 tancada correctament.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        let alies = prompt("Identificat amb un alies")
-		console.log(alies);
-        window.location.assign("./html/game.html");
-    });
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
+var c1 = $('#play')
+var c2 = $('#options')
+var c3 = $('#saves')
+var c4 = $('#exit')
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        alert("Opcions:");
-    });
+c1.on('click', 
+function(){
+    let alies = prompt("Identificat amb un alies")
+	console.log(alies);
+    window.location.assign("./html/game.html");
+});
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        alert("Partides anteriors:");
-    });
+c2.on('click', 
+function(){
+    alert("Opcions:");
+});
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
-		alert("Sortint....");
-        console.warn("Carregant sortida");
-    });
+c3.on('click', 
+function(){
+	alert("Partides anteriors:");
+});
+
+c4.on('click', 
+function(){
+	alert("Sortint....");
+	console.warn("Carregant sortida");
 });
