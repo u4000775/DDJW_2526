a4520a1c2a3465bf7613c4dcbf6e2d8a9e78e7fd
u6103687 <pautano2005@gmail.com>
2026-03-15 16:59:21 +0100

    Main JQUERY

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
+$(document).ready(function() {
+    $('#play').on('click', function() {
         window.location.assign("./html/game.html");
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    $('#options').on('click', function() {
         console.error("Opció no implementada");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    $('#saves').on('click', function() {
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    $('#exit').on('click', function() {
         console.warn("No es pot sortir!");
     });
 });
