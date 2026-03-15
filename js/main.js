053cda9b7383762a37d9d6da216bf26c47c852f4
Bru Postigo <u6105606@campus.udg.edu>
2026-03-15 22:18:16 +0100

    Issue 1 tancada correctament

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
+$(document).ready(function() {
+    $('#play').on('click', function() {
         let aliesJugador = prompt("Introdueix el teu àlies per jugar:");
         console.log(aliesJugador);
         window.location.assign("./html/game.html");
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    $('#options').on('click', function() {
         console.error("Opció no implementada");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    $('#saves').on('click', function() {
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    $('#exit').on('click', function() {
         console.warn("No es pot sortir!");
     });
 });
