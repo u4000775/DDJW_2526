c5474a6abec7ee84c5e800b2a24a5eeac8fab7c5
BlastES <72162714+BlastES@users.noreply.github.com>
2026-03-15 21:09:07 +0100

    ISSUE #1 tancada correctament

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
+import {$} from "../library/jquery-4.0.0.slim.module.min.js";
+$(document).ready(function() {
+    var play = $('#play');
+    var options = $('#options');
+    var saves = $('#saves');
+    var exit = $('#exit');
+
+    play.click(function(){
         window.location.assign("./html/game.html");
     });
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
+    options.click(function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
+    saves.on('click', function(){
         console.error("Opció no implementada");
     });
 
-    document.getElementById('exit').addEventListener('click', 
-    function(){
+    exit.on('click', function(){
         console.warn("No es pot sortir!");
     });
 });
