8c1d8058ea5fede1a760c8f476b39ceec9f83d6e
Antonio Rodriguez <antonio.rodriguez@udg.edu>
2026-03-19 22:44:29 +0100

    Afegin recursos i mostrem les primeres cartes

--- js/scene.js
+++ js/scene.js
@@ -1,11 +1,28 @@
+import {clickCard, gameItems, selectCards, startGame, initCard, saveGame} from "./memory.js";
+
 export class PlayScene extends Phaser.Scene{
     constructor(){
         super('PlayScene');
+        this.resources = [];
+        selectCards();
+        this.cards = [...gameItems];
     }
 
-    preload() { }
+    preload() {
+        this.cards.forEach(card => {
+            if (!this.resources.includes(card))
+                this.resources.push(card);
+            initCard(val => card = val); 
+        });
+        this.resources.push("../resources/back.png");
+        this.resources.forEach(r=>this.load.image(r,r)); // Primer paràmetre nom Segon paràmetre direcció
+    }
 
-    create() { }
+    create() { 
+        startGame();
+        this.cameras.main.setBackgroundColor(0xBFFCFF);
+        this.cards.forEach((c, i)=> this.add.sprite(250 + 100*i, 300, c));
+    }
 
     update() { }
 }
